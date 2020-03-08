79475f47665c9dadfbada736a9cb29fd7e6e71bb
dementedmax <43807330+dementedmax@users.noreply.github.com>
2020-03-08 18:42:28 +0600

    Создан компонент топбара сайта

--- nika-front/src/app/app.module.ts
+++ nika-front/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
