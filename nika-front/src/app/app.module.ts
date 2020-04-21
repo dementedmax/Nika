d2546e2d8d54079f6cb8bd6bee43f89e574ba62d
dementedmax <43807330+dementedmax@users.noreply.github.com>
2020-04-21 13:12:26 +0600

    Вёрстка фильтров
    Свёрстаны фильтры. Функционал не реализован.

--- nika-front/src/app/app.module.ts
+++ nika-front/src/app/app.module.ts
@@ -4,21 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { DbPageComponent } from './components/db-page/db-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    NavbarComponent
+    NavbarComponent,
+    DbPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
