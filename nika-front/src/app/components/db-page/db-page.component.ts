d1de168ea453998296b289a5d8c16b3911138b4b
dementedmax <43807330+dementedmax@users.noreply.github.com>
2020-04-24 17:04:34 +0600

    Правки
    Внесены правки. Убран пагинатор и исправлена ошибка отображения списка марок и производителей.

--- nika-front/src/app/components/db-page/db-page.component.ts
+++ nika-front/src/app/components/db-page/db-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
@@ -67,10 +66,7 @@ export class DbPageComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
   }
 
 }
